6d28e7fd3d932d64bd566b4213b6c218ea8d7cbf
jasper <jasperzhou1129@gmail.com>
2021-06-24 21:50:07 -0400

    http status

---
+++ src/http/Status.java
@@ -0,0 +1,15 @@
+package http;
+
+public interface Status {
+    int HTTP_OK = 200;
+    int HTTP_CREATED = 201;
+    int HTTP_ACCEPTED = 202;
+    int HTTP_NO_CONTENT = 204;
+    int HTTP_BAD_REQUEST = 400;
+    int HTTP_FORBIDDEN = 403;
+    int HTTP_BAD_METHOD = 405;
+    int HTTP_VERSION = 505;
+
+
+
+}
