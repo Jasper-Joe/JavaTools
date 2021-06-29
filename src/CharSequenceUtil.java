d42f4eafa08c44699d049606ae7e6741544bb879
jasper <jasperzhou1129@gmail.com>
2021-06-29 19:10:42 -0400

    trim method

--- src/CharSequenceUtil.java
+++ src/CharSequenceUtil.java
@@ -13,4 +13,74 @@ public class CharSequenceUtil {
 
         return true;
     }
+
+    public static String trim(CharSequence str) {
+        return (str == null) ? null : trim(str, 0);
+    }
+
+    public static String trimEnd(CharSequence str) {
+        return trim(str, 1);
+    }
+
+    /**
+     *
+     * @param str
+     * @param mode {-1} means trimStart, {0} means trim all, {1} means trimEnd
+     * @return
+     */
+    public static String trim(CharSequence str, int mode) {
+        String result;
+        if (str == null) {
+            result = null;
+        } else {
+            int length = str.length();
+            int start = 0;
+            int end = length;
+            if (mode <= 0) {
+                while ((start < end) && (CharUtil.isBlankChar(str.charAt(start)))) {
+                    start++;
+                }
+            }
+            if (mode >= 0) {
+                while ((start < end) && (CharUtil.isBlankChar(str.charAt(end - 1)))) {
+                    end--;
+                }
+            }
+
+            if ((start > 0) || (end < length)) {
+                result = str.toString().substring(start, end);
+            } else {
+                result = str.toString();
+            }
+        }
+        return result;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
