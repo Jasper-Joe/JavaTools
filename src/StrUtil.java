4fba9cc68e31319f0949e0b35d3a88a19858d0f4
jasper <jasperzhou1129@gmail.com>
2021-06-29 19:13:45 -0400

    trim in str

--- src/StrUtil.java
+++ src/StrUtil.java
@@ -1,2 +1,14 @@
-public class StrUtil implements StrPool{
+public class StrUtil extends CharSequenceUtil implements StrPool{
+    public static void trim(String[] strs) {
+        if (strs == null) {
+            return;
+        }
+        String str;
+        for (int i = 0; i < strs.length; i++) {
+            str = strs[i];
+            if (str != null) {
+                strs[i] = trim(str);
+            }
+        }
+    }
 }
