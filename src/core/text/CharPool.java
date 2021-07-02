b1a3bd131a04e8a6df835c9e931e59c6949b3dc3
jasper <jasperzhou1129@gmail.com>
2021-07-02 19:09:46 -0400

    pool

---
+++ src/core/text/CharPool.java
@@ -0,0 +1,10 @@
+package core.text;
+
+public interface CharPool {
+    char SPACE = ' ';
+    char SLASH = '/';
+    char DASHED = '-';
+    char BACKSLASH = '\\';
+    char COMMA = ',';
+    char DELIM_START = '{';
+}
---
+++ src/core/text/StrPool.java
@@ -0,0 +1,13 @@
+package core.text;
+
+public interface StrPool {
+    char C_SPACE = CharPool.SPACE;
+    char C_COMMA = CharPool.COMMA;
+    String DOT = ".";
+    String BACKSLASH = "\\";
+    String COMMA = ",";
+    String EMPTY_JSON = "{}";
+    String DELIM_START = "{";
+    char C_DELIM_START = CharPool.DELIM_START;
+    char C_BACKSLASH = CharPool.BACKSLASH;
+}
