8a65b80f80883d45be61c94d9391a6bc60615da2
jasper <jasperzhou1129@gmail.com>
2021-07-06 18:30:13 -0400

    number util

---
+++ src/core/util/NumberUtil.java
@@ -0,0 +1,16 @@
+package core.util;
+
+import java.math.BigDecimal;
+
+public class NumberUtil {
+    public static boolean equals(BigDecimal bigNum1, BigDecimal bigNum2) {
+        if (bigNum1 == bigNum2) {
+            return true;
+        }
+
+        if (bigNum1 == null || bigNum2 == null) {
+            return false;
+        }
+        return bigNum1.compareTo(bigNum2) == 0;
+    }
+}
