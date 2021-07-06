2f372401586a7962646a3003ebea4e160de54146
jasper <jasperzhou1129@gmail.com>
2021-07-06 11:28:15 -0400

    map util

---
+++ src/core/map/MapUtil.java
@@ -0,0 +1,11 @@
+package core.map;
+
+import java.util.Map;
+
+public class MapUtil {
+    public static final int DEFAULT_INITIAL_CAPACITY = 16;
+    public static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    public static boolean isEmpty(Map<?, ?> map) {
+        return map == null || map.isEmpty();
+    }
+}
